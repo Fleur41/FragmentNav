465f4ecb5cc21fdbdd67921fef189946a8bcc278
Fleur41 <Muriithikings456@gmail.com>
2024-12-10 13:51:21 +0300

    updated FragmentThree and  enabling navigation from one that to FragmentOne.

--- app/src/main/java/com/simon/fragmentsnav/MainActivity.kt
+++ app/src/main/java/com/simon/fragmentsnav/MainActivity.kt
@@ -2,10 +2,13 @@ package com.simon.fragmentsnav
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.simon.fragmentsnav.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(R.layout.activity_main)
     }
 }
